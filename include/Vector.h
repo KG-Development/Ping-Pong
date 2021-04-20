e548b775367b033bd55f41114604d57eca200adb
k05j <kilian.halwachs@gmx.at>
2021-04-20 17:31:36 +0200

    vector

---
+++ include/Vector.h
@@ -0,0 +1,16 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+
+class Vector {
+    public:
+        int x, y;
+        Vector(int x, int y);
+        virtual ~Vector();
+
+    protected:
+
+    private:
+};
+
+#endif // VECTOR_H
---
+++ src/Vector.cpp
@@ -0,0 +1,9 @@
+#include "Vector.h"
+
+Vector::Vector(int x, int y) {
+    //ctor
+}
+
+Vector::~Vector() {
+    //dtor
+}
